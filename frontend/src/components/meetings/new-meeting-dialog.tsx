f4fcfbde4a7175c6910f4353c648d969415a6494
Ashmith Shetty <ashmithnshetty@gmail.com>
2026-07-11 16:59:47 +0530

    fix: add overflow scrolling to new meeting dialog to prevent tall transcript inputs from blocking the create button

--- frontend/src/components/meetings/new-meeting-dialog.tsx
+++ frontend/src/components/meetings/new-meeting-dialog.tsx
@@ -102,7 +102,7 @@ export function NewMeetingDialog({ open, onOpenChange, onSuccess }: NewMeetingDi
 
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent className="sm:max-w-[500px]">
+      <DialogContent className="sm:max-w-[500px] max-h-[90vh] overflow-y-auto">
         <DialogHeader>
           <DialogTitle>Create New Meeting</DialogTitle>
         </DialogHeader>
